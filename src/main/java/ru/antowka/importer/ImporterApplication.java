22a55322e71c6c47db52a06f418f0d55d83d8589
Склемин Вадим Сергеевич <XSW@zaq1>
2022-10-08 11:47:35 +0300

    Правки

--- src/main/java/ru/antowka/importer/ImporterApplication.java
+++ src/main/java/ru/antowka/importer/ImporterApplication.java
@@ -1,11 +1,8 @@
 package ru.antowka.importer;
 
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 import java.io.*;
 
@@ -13,6 +10,7 @@ import java.io.*;
 public class ImporterApplication {
 
     public static void main(String[] args) throws FileNotFoundException {
+
         SpringApplication.run(ImporterApplication.class, args);
     }
 }
