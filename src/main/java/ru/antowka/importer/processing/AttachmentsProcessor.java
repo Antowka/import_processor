0ce0f79c856cc51cb2384eab9f1cdb0dbeee9d8f
Anton Nikanorov <anikanorov@blogic.ru>
2022-10-08 13:10:40 +0300

    123

--- src/main/java/ru/antowka/importer/processing/AttachmentsProcessor.java
+++ src/main/java/ru/antowka/importer/processing/AttachmentsProcessor.java
@@ -1,14 +1,17 @@
 package ru.antowka.importer.processing;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.antowka.importer.model.Attachment;
-import ru.antowka.importer.model.Attachments;
 import ru.antowka.importer.model.NodeModel;
-import ru.antowka.importer.service.SearchAttachmentServiceImpl;
+import ru.antowka.importer.service.SearchAttachmentService;
 
 import java.util.List;
 
 public class AttachmentsProcessor implements ItemProcessor<NodeModel, NodeModel> {
+
+    @Autowired
+    private SearchAttachmentService searchAttachmentService;
 
     /**
      * Дополняем модельку вложениями
@@ -19,8 +22,7 @@ public class AttachmentsProcessor implements ItemProcessor<NodeModel, NodeModel>
      */
     @Override
     public NodeModel process(NodeModel model) throws Exception {
-        SearchAttachmentServiceImpl service = new SearchAttachmentServiceImpl();
-        List<Attachment> attachments = service.searchAttachmentsDocument(model.getNodeRef());
+        List<Attachment> attachments = searchAttachmentService.searchAttachmentsDocument(model.getNodeRef());
         model.setAttachmentsData(attachments);
         return model;
     }
